c815695e6b0c24d215df929eab8167f542f35dde
Cameron Probert <c.probert@hotmail.co.nz>
2015-05-02 16:01:42 +1200

    Main method now handles arguments passed in from the command line.
    Currently the command '-verbose' works. This makes the program print
    more information to the console.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,27 @@
 package main;
 
-public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import main.logic.Monitor;
 
+/**
+ * This is the entry point of the program, it is an empty class, used just to
+ * start the program
+ * 
+ * @author Cameron Probert
+ *
+ */
+public class Main {
+	public static void main(String[] arguments) {
+		boolean verbose;
+		if (arguments.length > 0) {
+			if (arguments[0].equals("-verbose")) {
+				verbose = true;
+			} else {
+				System.out.println();
+				verbose = false;
+			}
+		} else {
+			verbose = false;
+		}
+		new Monitor(verbose);
 	}
 }
