46b26c26f9f7ecf9365da98ca3355eee605c8766
Cameron Probert <c.probert@hotmail.co.nz>
2015-05-02 16:02:45 +1200

    Added the Monitor class. This will handle logic for user input.

---
+++ src/main/logic/Monitor.java
@@ -0,0 +1,18 @@
+package main.logic;
+
+/**
+ * The monitor is the main logic class of the program. It handles user input
+ * passed in from the GUI and will .
+ * 
+ * @author Cameron Probert
+ *
+ */
+public class Monitor {
+
+	private final boolean verbose;
+
+	public Monitor(boolean verbose) {
+		this.verbose = verbose;
+	}
+
+}
