78e67d0d6b57c50ff5be3a83ac2aae809b113083
Cameron Probert <c.probert@hotmail.co.nz>
2015-05-03 17:43:31 +1200

    Added the internal working of the clerk class as planned in class diagram

--- src/main/logic/Clerk.java
+++ src/main/logic/Clerk.java
@@ -1,14 +1,92 @@
 package main.logic;
 
+/**
+ * A clerk should be logged on to the program at all times, it is the programs
+ * recorded identity of the person currently using it.
+ * 
+ * @author Cameron Probert
+ *
+ */
 public class Clerk {
 
-	public Clerk() {
-		
+	// Name and ID are final fields so they cannot be changed after creating the
+	// clerk
+	protected final String name;
+	protected final String id;
+
+	// Password is not final so that it can be changed at a later date if
+	// needed
+	protected String password;
+
+	/**
+	 * Creates a new Clerk with the given strings
+	 * 
+	 * @param name
+	 * @param id
+	 * @param password
+	 */
+	public Clerk(String name, String id, String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
 	}
 
-	public boolean equals(String id, String password) {
+	/**
+	 * Returns the name of the clerk
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the id of the Clerk
+	 * 
+	 * @return
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Returns true of the given id and password match this user's id and
+	 * password
+	 * 
+	 * @param id
+	 * @param password
+	 * @return
+	 */
+	public boolean isUser(String id, String password) {
 		// TODO Auto-generated method stub
-		return false;
+		return this.id.equals(id) && this.password.equals(password);
 	}
 
+	/**
+	 * Returns the users credentials without the password as that is private
+	 */
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Type: Clerk");
+		sb.append("Name: " + name);
+		sb.append("\nID: " + id);
+		return sb.toString();
+	}
+
+	/**
+	 * Returns the users credentials including the password. This should only
+	 * ever be used for debugging
+	 * 
+	 * @return
+	 */
+	public String debugToString() {
+		StringBuilder sb = new StringBuilder();
+		System.err
+				.println("debugToString() method has been called. This should never happen in the final version of this program");
+		sb.append("Type: Clerk");
+		sb.append("Name: " + name);
+		sb.append("\nID: " + id);
+		sb.append("\nPassword: " + password);
+		return sb.toString();
+	}
 }
