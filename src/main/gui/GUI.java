997c2b64d99401db7feed9f49b3dab85b0b6e7c2
Claire Sun <claire.szh@gmail.com>
2015-05-05 21:22:32 +1200

    Create JFrame and the draft of the log in panel

---
+++ src/main/gui/GUI.java
@@ -0,0 +1,100 @@
+package main.gui;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JTextField;
+
+
+
+/**
+ * The GUI is responsible for setting up the KPSmart system entry. This class generates
+ * different options to let player choose different game mode. The class also
+ * records players's information and pass to other classes.
+ *
+ */
+
+public class GUI {
+
+	private JFrame frame;	// this is the frame the KPSmart system will be shown on
+	private JLayeredPane layeredPane;	// this is used to add panel onto the frame
+	private Panel backgroundPanel;
+
+	// the dimension of the frame
+	private static int width = 900;
+	private static int height = 770;
+	
+	// textFields on all panels
+	private JTextField usernameTextF;
+	private JTextField passwordTextF;
+
+	public String username;	// the entered name of the player in multiple-player mode
+	public String password;
+	
+	public GUI() {
+		setUp();
+	}
+
+	private void setUp() {
+		frame = new JFrame();
+		frame.setTitle("KPSmart System");
+		frame.getRootPane().setBackground(new Color(141, 174, 240));
+//		frame.getRootPane().setBackground(new Color(115, 129, 240));		
+		frame.setSize(width, height);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+
+		// add the background image to frame
+		layeredPane = new JLayeredPane();
+//		backgroundPanel = new BackgroundPanel(this);
+//		layeredPane.add(backgroundPanel, JLayeredPane.DEFAULT_LAYER);
+
+		frame.setLayeredPane(layeredPane);
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.setVisible( true );
+
+		usernameTextF = new JTextField(18);
+		passwordTextF = new JTextField(18);
+		
+		LoginGUI loginGUI = new LoginGUI(this);
+		addPanel(loginGUI);
+	}
+	
+	protected void addPanel(Panel panel){
+		layeredPane.add(panel, JLayeredPane.MODAL_LAYER);
+		frame.repaint();
+	}
+	
+	protected void removePanel(Panel panel){
+		layeredPane.remove(panel);
+		frame.repaint();
+	}
+	
+	protected Panel getBackgroundPanel(){
+		return backgroundPanel;
+	}
+	
+	public JTextField getUsername() {
+		return usernameTextF;
+	}
+	
+	public JTextField getPassword() {
+		return passwordTextF;
+	}
+	
+	public void setUsername(String text) {
+		username = text;
+	}
+	
+	public void setPassword(String text) {
+		password = text;
+	}
+	
+	
+	
+	
+	public static void main(String[] args){
+		new GUI();
+	}
+}
--- src/main/gui/LoginGUI.java
+++ src/main/gui/LoginGUI.java
@@ -1,5 +1,143 @@
 package main.gui;
 
-public class LoginGUI {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ * The JoinServerPanel class is a JPanel which is represented on 
+ * the frame once the player choose to start a game in a server. 
+ * JoinServerPanel class is responsible for letting player enter 
+ * the server information and then start the game.
+ * 
+ */
+public class LoginGUI extends Panel{
+
+	// buttons on the panel
+	private JButton reset;
+	private JButton login;
+
+	public LoginGUI(GUI gui) {
+		super(gui);
+		setBounds(250, 320, 400, 250);	
+	}
+
+	@Override
+	protected void setUpComponents() {
+		// labels, textFields and button used on joinServerPanel
+		//		JLabel nameP = new JLabel("Username : ");
+		//		nameP.setPreferredSize(new Dimension(150, 60));
+		//		nameP.setFont(new Font("Arial", Font.PLAIN, 20));
+		//		nameP.setForeground(new Color(0, 135, 200).brighter());
+
+		gui.getUsername().setText("Username");
+		gui.getUsername().setPreferredSize(new Dimension(250, 50));
+		gui.getUsername().setFont(new Font("Arial", Font.PLAIN, 20));
+		gui.getUsername().setForeground(new Color(130, 130, 130));
+		gui.getUsername().setBackground(new Color(50, 50, 50));
+//		gui.getUsername().setHorizontalAlignment(JTextField.CENTER);
+		gui.getUsername().setBorder(null);
+		
+		//		JLabel name = new JLabel("Password : ");
+		//		name.setPreferredSize(new Dimension(150, 60));
+		//		name.setFont(new Font("Arial", Font.PLAIN, 20));
+		//		name.setForeground(new Color(0, 135, 200).brighter());
+
+		gui.getPassword().setText("Password");
+		gui.getPassword().setPreferredSize(new Dimension(250, 50));
+		gui.getPassword().setFont(new Font("Arial", Font.PLAIN, 20));
+		gui.getPassword().setForeground(new Color(130, 130, 130));
+		gui.getPassword().setBackground(new Color(50, 50, 50));
+		gui.getPassword().setBorder(null);
+
+		reset = new JButton("Reset");
+		login = new JButton("Login");
+
+		JLabel space1 = new JLabel("");
+		space1.setPreferredSize(new Dimension(350, 15));
+
+		JLabel space2 = new JLabel("");
+		space2.setPreferredSize(new Dimension(350, 15));
+
+		JLabel space3 = new JLabel("");
+		space3.setPreferredSize(new Dimension(20, 60));
+
+		add(gui.getUsername());
+		add(space1);
+		add(gui.getPassword());
+		add(space2);
+		setButtonStyle(reset, 137, Color.RED);
+		add(space3);
+		setButtonStyle(login, 137, Color.GREEN);
+	}
+
+	@Override
+	protected void addListenner() {
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				JButton button = (JButton) ae.getSource();
+				if(button == login){	// if button Start is clicked, joinServerPanel will be removed and multiple-player mode game will be started
+					if(!gui.getUsername().getText().equals("Username") && !gui.getPassword().getText().equals("Password")){
+						gui.setUsername(gui.getUsername().getText());
+						//						gui.setStrServerNameC(gui.getServerNameC().getText());
+						//						if(isIPAdd(gui.getStrServerNameC())){
+						//							gui.removePanel(JoinServerPanel.this);
+						//							gui.removePanel(gui.getBackgroundPanel());					
+						//							gui.startGame2();
+						//						}
+						System.out.println("username: " + gui.getUsername().getText());
+						System.out.println("password: " + gui.getPassword().getText());
+					}}}
+		});
+
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				JButton button = (JButton) ae.getSource();
+				if(button == reset){	// if button Start is clicked, joinServerPanel will be removed and multiple-player mode game will be started
+					gui.getUsername().setText("Username");
+					gui.getPassword().setText("Password");
+					gui.getUsername().setForeground(new Color(130, 130, 130));
+					gui.getPassword().setForeground(new Color(130, 130, 130));
+				}
+			}
+		});
+
+		gui.getUsername().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gui.setUsername(gui.getUsername().getText());	// get the player name player entered from the textField
+			}
+		});
+
+		gui.getUsername().addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (gui.getUsername().getText().equals("Username")){
+					gui.getUsername().setText("");
+					gui.getUsername().setForeground(new Color(255, 255, 255));
+				}
+			}
+		});
+
+		gui.getPassword().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gui.setPassword(gui.getUsername().getText());	// get the player name player entered from the textField
+			}
+		});
+
+		gui.getPassword().addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (gui.getPassword().getText().equals("Password")){
+					gui.getPassword().setText("");
+					gui.getPassword().setForeground(new Color(255, 255, 255));
+				}
+			}
+		});
+	}
 }
