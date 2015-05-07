9228cd9e090839726d642ea0c93b6ad89f544014
francinebc <francinebc.mail@gmail.com>
2015-05-07 16:27:12 +1200

    Satrted adding dom attributes to saving the xml file

--- src/main/fileio/SaveXML.java
+++ src/main/fileio/SaveXML.java
@@ -2,6 +2,7 @@ package main.fileio;
 
 import java.util.List;
 import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,9 +16,12 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import main.events.BusinessEvent;
+import main.events.*;
 
 public class SaveXML {
+
+	private Document doc;
+	private Element rootElement;
 
 
 	public SaveXML(List<BusinessEvent> events) {
@@ -27,38 +31,36 @@ public class SaveXML {
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
 			// root elements
-			Document doc = docBuilder.newDocument();
+			doc = docBuilder.newDocument();
 
-			Element rootElement = doc.createElement("business events");
+			rootElement = doc.createElement("business events");
 			doc.appendChild(rootElement);
 
 
 			for(BusinessEvent event: events){
 				if(event instanceof CustomerPriceChange){
-					saveCustomerPriceChange();
+					saveCustomerPriceChange((CustomerPriceChange)event);
 				}
 				else if (event instanceof OpenNewRoute){
-					saveOpenNewRoute();
+					saveOpenNewRoute((OpenNewRoute)event);
 				}
 				else if (event instanceof DeleteRoute){
-					saveDeleteRoute();
+					saveDeleteRoute((DeleteRoute)event);
 				}
 				else if (event instanceof TransportUpdate){
-					saveTransportUpdate();
+					saveTransportUpdate((TransportUpdate)event);
 				}
 				else if (event instanceof MailDelivery){
-					saveMailDelivery();
+					saveMailDelivery((MailDelivery)event);
 				}
 			}
 
-			// staff elements
-			Element staff = doc.createElement("Staff");
-			rootElement.appendChild(staff);
+
 
 			// set attribute to staff element
 			Attr attr = doc.createAttribute("id");
 			attr.setValue("1");
-			staff.setAttributeNode(attr);
+			//staff.setAttributeNode(attr);
 
 			// shorten way
 			// staff.setAttribute("id", "1");
@@ -66,22 +68,22 @@ public class SaveXML {
 			// firstname elements
 			Element firstname = doc.createElement("firstname");
 			firstname.appendChild(doc.createTextNode("yong"));
-			staff.appendChild(firstname);
+			//staff.appendChild(firstname);
 
 			// lastname elements
 			Element lastname = doc.createElement("lastname");
 			lastname.appendChild(doc.createTextNode("mook kim"));
-			staff.appendChild(lastname);
+			//staff.appendChild(lastname);
 
 			// nickname elements
 			Element nickname = doc.createElement("nickname");
 			nickname.appendChild(doc.createTextNode("mkyong"));
-			staff.appendChild(nickname);
+			//staff.appendChild(nickname);
 
 			// salary elements
 			Element salary = doc.createElement("salary");
 			salary.appendChild(doc.createTextNode("100000"));
-			staff.appendChild(salary);
+			//staff.appendChild(salary);
 
 			// write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -96,11 +98,11 @@ public class SaveXML {
 
 			System.out.println("File saved!");
 
-		  } catch (ParserConfigurationException pce) {
+		} catch (ParserConfigurationException pce) {
 			pce.printStackTrace();
-		  } catch (TransformerException tfe) {
+		} catch (TransformerException tfe) {
 			tfe.printStackTrace();
-		  }
+		}
 
 
 
@@ -108,28 +110,65 @@ public class SaveXML {
 
 	}
 
-	private void saveMailDelivery() {
+	private void saveMailDelivery(MailDelivery event) {
+		// staff elements
+		Element mail = doc.createElement("Staff");
+		rootElement.appendChild(mail);
+
+		saveRoute(event, mail);
+
+		Element origin = doc.createElement("origin");
+		origin.appendChild(doc.createTextNode(event.getOrigin()));
+		mail.appendChild(origin);
+
+		Element destination = doc.createElement("destination");
+		destination.appendChild(doc.createTextNode(event.getDestination()));
+		mail.appendChild(destination);
+
+		Element weight = doc.createElement("weight");
+		weight.appendChild(doc.createTextNode(String.valueOf(event.getWeight())));
+		mail.appendChild(weight);
+
+		Element volume = doc.createElement("volume");
+		volume.appendChild(doc.createTextNode(String.valueOf(event.getVolume())));
+		mail.appendChild(volume);
+
+		Element priority = doc.createElement("priority");
+		priority.appendChild(doc.createTextNode(String.valueOf(event.getPriority())));
+		mail.appendChild(priority);
+
+		Element revenue = doc.createElement("revenue");
+		revenue.appendChild(doc.createTextNode(String.valueOf(event.getRevenue())));
+		mail.appendChild(revenue);
+
+		Element timeTaken = doc.createElement("time taken");
+		timeTaken.appendChild(doc.createTextNode(String.valueOf(event.getTimeTaken())));
+		mail.appendChild(timeTaken);
+
+
+	}
+
+	private void saveTransportUpdate(TransportUpdate event) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void saveTransportUpdate() {
+	private void saveDeleteRoute(DeleteRoute event) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void saveDeleteRoute() {
+	private void saveOpenNewRoute(OpenNewRoute event) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void saveOpenNewRoute() {
+	private void saveCustomerPriceChange(CustomerPriceChange event) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void saveCustomerPriceChange() {
-		// TODO Auto-generated method stub
+	private void saveRoute(BusinessEvent event, Element elm){
 
 	}
 
