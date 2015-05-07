53b33bc85a0e3567dccd0f2778d8947b837fdeed
Daphne <daphnewangnz@gmail.com>
2015-05-07 15:14:37 +1200

    testing

--- src/main/fileio/NoRegisteredUsersException.java
+++ src/main/fileio/NoRegisteredUsersException.java
@@ -3,18 +3,18 @@ package main.fileio;
 /**
  * Exception thrown when there are no users to load in from the file containing
  * all users.
- * 
+ *
  * @author Cameron Probert
  *
  */
 public class NoRegisteredUsersException extends Exception {
 
-	// Automatically generated UID
+	// Automatically generated UID blah
 	private static final long serialVersionUID = 1006740381526100903L;
 
 	/**
 	 * Creates the exception with the given error message
-	 * 
+	 *
 	 * @param message
 	 */
 	public NoRegisteredUsersException(String message) {
