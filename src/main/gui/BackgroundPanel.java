a339aa9e0c655cf2aed54a9d5ce63525d8d7cf5c
Claire Sun <claire.szh@gmail.com>
2015-05-09 00:20:19 +1200

    Add image

--- src/main/gui/BackgroundPanel.java
+++ src/main/gui/BackgroundPanel.java
@@ -10,8 +10,6 @@ import javax.swing.JLabel;
  * The BackgroundPanel class is a JPanel which is represented 
  * as the background image on the frame. 
  * 
- * @author Zhiheng Sun,  ID: 300256273
- * 
  */
 public class BackgroundPanel extends Panel{
 	
@@ -25,22 +23,11 @@ public class BackgroundPanel extends Panel{
 	@Override
 	protected void setUpComponents() {
 		background = new ImageIcon("bg.jpg");
-		
-		Image scaledImage = background.getImage().getScaledInstance(900,770,Image.SCALE_SMOOTH);
+		Image scaledImage = background.getImage().getScaledInstance(900, 770, Image.SCALE_SMOOTH);
 		JLabel jl = new JLabel(new ImageIcon(scaledImage));
-		
 		add(jl);
 	}
 
 	@Override
 	protected void addListenner() {}
-
-	
-	
-	
-//	public void paintComponent(Graphics G) {
-//        super.paintComponent(G);
-//        G.drawImage(background.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
-//    }
-	
 }
