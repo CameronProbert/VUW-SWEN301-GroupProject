0ca8432a4ba26aeb4d51923c17bea2e4603b6923
francinebc <francinebc.mail@gmail.com>
2015-05-09 10:29:00 +1200

    Added routes to the busniess event

--- src/main/events/BusinessEvent.java
+++ src/main/events/BusinessEvent.java
@@ -1,20 +1,23 @@
 package main.events;
 
+import java.util.List;
+
 import main.logic.Clerk;
+import main.logic.Route;
 /**
  * Abstract class to describe all business events. Business events are used for statistic
  * computation and they are displayed in the xml file
- * 
+ *
  * @author Francine
  *
  */
 
 public abstract class BusinessEvent {
 
-	protected Clerk clerk; 
+	protected Clerk clerk;
 	protected String date;
-	// private Route route;  // this will be added when a route class is added
-	
+	private List<Route> routes;  // this will be added when a route class is added
+
 	/**
 	 * returns the date the event was created
 	 * @return date
@@ -22,10 +25,18 @@ public abstract class BusinessEvent {
 	public String dateDone(){
 		return date;
 	}
-	
+
 	/**
 	 * returns the xml description of the business event which is saved to file
 	 * @return
 	 */
 	public abstract String toXML();
+
+	/**
+	 * returns the routes associated with this business event
+	 * @return
+	 */
+	public List<Route> getRoutes(){
+		return this.routes;
+	}
 }
--- src/main/fileio/SaveXML.java
+++ src/main/fileio/SaveXML.java
@@ -17,6 +17,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import main.events.*;
+import main.logic.Route;
 import main.logic.Route.DaysOfWeek;
 
 public class SaveXML {
@@ -185,59 +186,62 @@ public class SaveXML {
 
 	private void saveRoute(BusinessEvent event, Element elm){
 
-		// TODO will need to go through and uncomment a lot of this also check for ints that need to be parsed to Strings
+		for(Route r: event.getRoutes()){
+			// TODO will need to go through and uncomment a lot of this also check for ints that need to be parsed to Strings
 
-		Element route = doc.createElement("route");
-		elm.appendChild(route);
+			Element route = doc.createElement("route");
+			elm.appendChild(route);
 
-		Element origin = doc.createElement("origin");
-		//origin.appendChild(doc.createTextNode(event.getRoute().getOrigin()));
-		route.appendChild(origin);
+			Element origin = doc.createElement("origin");
+			//origin.appendChild(doc.createTextNode(event.getRoute().getOrigin()));
+			route.appendChild(origin);
 
-		Element destination = doc.createElement("destination");
-		//destination.appendChild(doc.createTextNode(event.getRoute().getDestination()));
-		route.appendChild(destination);
+			Element destination = doc.createElement("destination");
+			//destination.appendChild(doc.createTextNode(event.getRoute().getDestination()));
+			route.appendChild(destination);
 
-		Element transportType = doc.createElement("Transport Type");
-		//transportType.appendChild(doc.createTextNode(event.getRoute().getTransportType()));
-		route.appendChild(transportType);
+			Element transportType = doc.createElement("Transport Type");
+			//transportType.appendChild(doc.createTextNode(event.getRoute().getTransportType()));
+			route.appendChild(transportType);
 
-		Element averageTime = doc.createElement("Average Time");
-		//averageTime.appendChild(doc.createTextNode(event.getRoute().getAverageTime()));
-		route.appendChild(averageTime);
+			Element averageTime = doc.createElement("Average Time");
+			//averageTime.appendChild(doc.createTextNode(event.getRoute().getAverageTime()));
+			route.appendChild(averageTime);
 
-		Element firmName = doc.createElement("Firm Name");
-		//firmName.appendChild(doc.createTextNode(event.getRoute().getFirmName()));
-		route.appendChild(firmName);
+			Element firmName = doc.createElement("Firm Name");
+			//firmName.appendChild(doc.createTextNode(event.getRoute().getFirmName()));
+			route.appendChild(firmName);
 
-		Element gramTransport = doc.createElement("Cost/gram for transport");
-		//gramTransport.appendChild(doc.createTextNode(event.getRoute().getPricePerGramTransport()));
-		route.appendChild(gramTransport);
+			Element gramTransport = doc.createElement("Cost/gram for transport");
+			//gramTransport.appendChild(doc.createTextNode(event.getRoute().getPricePerGramTransport()));
+			route.appendChild(gramTransport);
 
-		Element volumeTransport = doc.createElement("Cost/volume for transport");
-		//volumeTransport.appendChild(doc.createTextNode(event.getRoute().getPricePerVolumeTransport()));
-		route.appendChild(volumeTransport);
+			Element volumeTransport = doc.createElement("Cost/volume for transport");
+			//volumeTransport.appendChild(doc.createTextNode(event.getRoute().getPricePerVolumeTransport()));
+			route.appendChild(volumeTransport);
 
-		Element gramCustomer = doc.createElement("Cost/gram to customer");
-		//gramCustomer.appendChild(doc.createTextNode(event.getRoute().getPricePerGramCustomer()));
-		route.appendChild(gramCustomer);
+			Element gramCustomer = doc.createElement("Cost/gram to customer");
+			//gramCustomer.appendChild(doc.createTextNode(event.getRoute().getPricePerGramCustomer()));
+			route.appendChild(gramCustomer);
 
-		Element volumeCustomer = doc.createElement("Cost/volume to customer");
-		//volumeCustomer.appendChild(doc.createTextNode(event.getRoute().getPricePerVolumeCustomer()));
-		route.appendChild(volumeCustomer);
+			Element volumeCustomer = doc.createElement("Cost/volume to customer");
+			//volumeCustomer.appendChild(doc.createTextNode(event.getRoute().getPricePerVolumeCustomer()));
+			route.appendChild(volumeCustomer);
 
-		// TODO this may not be the best way to display this information
-		Element departureDays = doc.createElement("Departure Days");
-		route.appendChild(departureDays);
-		for(DaysOfWeek day: event.getRoute().getDays()){
-			Element dayElm = doc.createElement("day");
-			dayElm.appendChild(doc.createTextNode(day.toString()));
-			departureDays.appendChild(dayElm);
+			// TODO this may not be the best way to display this information
+			Element departureDays = doc.createElement("Departure Days");
+			route.appendChild(departureDays);
+			for(DaysOfWeek day : r.getDays()){
+				Element dayElm = doc.createElement("day");
+				dayElm.appendChild(doc.createTextNode(day.toString()));
+				departureDays.appendChild(dayElm);
+			}
+
+			Element departureFreq = doc.createElement("Departure Frequency");
+			//departureFreq.appendChild(doc.createTextNode(event.getRoute().getDepartureFrequency()));
+			route.appendChild(departureFreq);
+
 		}
-
-		Element departureFreq = doc.createElement("Departure Frequency");
-		//departureFreq.appendChild(doc.createTextNode(event.getRoute().getDepartureFrequency()));
-		route.appendChild(departureFreq);
 
 
 	}
